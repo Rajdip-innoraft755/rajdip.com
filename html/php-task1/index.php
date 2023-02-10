47ba450219d163853f4caf57964f022ccf80201d
Rajdip-innoraft755 <rajdip.roy@innoraft.com>
2023-02-10 10:08:17 +0530

    FT2023-55:php-task1 created.

---
+++ html/php-task1/index.php
@@ -0,0 +1,31 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>show date and time </title>
+        <link rel="stylesheet" href="css/style.css">
+        <style>            
+        </style>
+    </head>
+    <body>
+        <section class="details">
+            <div class="container">
+                <h1>welcome from rajdip</h1>
+                <form action="welcome.php"  method="POST">
+                    <div class="input-field">
+                        <span>FIRST NAME :</span> <input type="text" name="fname" placeholder="enter your first name">
+                    </div>
+                    <div class="input-field">
+                        <span>LAST NAME :</span> <input type="text"  name="mail" placeholder="enter your last name">
+                    </div>
+                    <a class="submit" href="action.php"><input type="submit"></a>
+                </form>  
+            </div>
+        </section>
+    </body>
+</html>
+  
+
+
+
